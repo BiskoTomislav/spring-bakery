93d6b54f14e92cdeaffa95b1f091a89a914999b3
Tomislav <tomislav.bisko@gmail.com>
2020-09-08 20:37:33 +0200

    Init commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+Gradle commands:
+* List tasks: **gradlew tasks**
+* Run app: **gradlew bootRun**
---
+++ src/test/java/hr/tomi/bakery/BakeryApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tomi.bakery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BakeryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
